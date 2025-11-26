528f967915ddac555849c07f07aa55b6039dab5e
codeBuddha <jamwujustyle@gmail.com>
2025-11-26 21:24:09 +0500

    system confs

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Lord's NixOS Flake Configuration";
+
+  inputs = {
+    # We use unstable for the latest packages (Go, VSCode, etc.)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+    
+      nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+    };
+  };
+}
